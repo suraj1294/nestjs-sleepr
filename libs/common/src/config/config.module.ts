0c77f131c6acdd092f250d135dc03c32de1ef319
ipalace <ipalace@ipalaces-MacBook-Pro.local>
2023-04-15 13:10:43 +0530

    configure database module

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule as NestjsConfigModule } from '@nestjs/config';
+import {
+  ConfigService,
+  ConfigModule as NestjsConfigModule,
+} from '@nestjs/config';
 
+import * as joi from 'joi';
 @Module({
-  imports: [NestjsConfigModule.forRoot()],
+  imports: [
+    NestjsConfigModule.forRoot({
+      validationSchema: joi.object({ MONGODB_URL: joi.string().required() }),
+    }),
+  ],
+  providers: [ConfigService],
+  exports: [ConfigService],
 })
 export class ConfigModule {}
--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '../config';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
 export class DatabaseModule {}
