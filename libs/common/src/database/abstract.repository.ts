43ce7dacc9b9196c131e127952a7455433eedcff
ipalace <ipalace@ipalaces-MacBook-Pro.local>
2023-04-15 22:21:54 +0530

    add reservations crud repository

--- libs/common/src/database/abstract.repository.ts
+++ libs/common/src/database/abstract.repository.ts
@@ -1,13 +1,13 @@
 import { Logger, NotFoundException } from '@nestjs/common';
 import { AbstractDocument } from './abstract.schema';
-import { FilterQuery, Model, Types } from 'mongoose';
+import { FilterQuery, Model, Types, UpdateQuery } from 'mongoose';
 
 export abstract class AbstractRepository<TDocument extends AbstractDocument> {
   protected abstract readonly logger: Logger;
 
   constructor(protected readonly model: Model<TDocument>) {}
 
-  async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
+  async create(document: UpdateQuery<TDocument>): Promise<TDocument> {
     const createdDocument = new this.model({
       ...document,
       _id: new Types.ObjectId(),
@@ -28,7 +28,7 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
 
   async findOneAndUpdate(
     filterQuery: FilterQuery<TDocument>,
-    update: TDocument,
+    update: UpdateQuery<TDocument>,
   ): Promise<TDocument> {
     const document = await this.model.findOneAndUpdate(filterQuery, update, {
       lean: true,
